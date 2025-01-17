5c203ee2064bb082b553cb936d55b9db9af29cc3
Thales Carrion <thalesdesenvolvedor@gmail.com>
2025-01-17 18:01:23 -0300

    update frontend

--- cadastro-frontend/src/app/models/produto.ts
+++ cadastro-frontend/src/app/models/produto.ts
@@ -1,16 +1,7 @@
 export class Produto {
-
   id!: number;
   name!: string;
   description!: string;
   price!: number;
   avaible!: boolean;
-
-  /*constructor(id: number, name:string, description: string, price: number, avaible: boolean){
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.avaible = avaible;
-  }*/
 }
